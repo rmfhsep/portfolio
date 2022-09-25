6d66b84c32f8efac9ab9bb21967eb6b7962d88eb
hoon <junghoon@babayo.tv>
2022-09-25 14:46:22 +0900

    자기소개 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,9 +45,10 @@ export default function Home() {
           </AnimationWrapper>
           {/* <span className="text-gray-300 sm:text-5xl">Frontend Developer</span> */}
           <p className="text-sm sm:text-base text-gray-700 dark:text-white">
-            Dimension of reality that makes change possible and understandable.
-            An indefinite and homogeneous environment in which natural events
-            and human existence take place.
+            문제를 발견하면, 끝까지 해결하려고 노력하는 엉덩이가 무거운
+            프론트엔드 개발자 김정훈입니다. 코드만 짜는 개발자가 아닌 문제를
+            해결하는 개발자가 되기 위해 노력하고 있습니다. 그러기 위해서,
+            커뮤니케이션을 무엇보다 중요시 생각합니다.
           </p>
           {/* <div className="flex mt-8">
             <a
